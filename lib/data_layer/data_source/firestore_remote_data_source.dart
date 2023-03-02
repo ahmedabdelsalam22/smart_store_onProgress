0f2f293ee62eaa46847f96906995ee22912fea38
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-03-02 16:47:31 +0200

    create uploadDataToFireStoreImpl

--- lib/data_layer/data_source/firestore_remote_data_source.dart
+++ lib/data_layer/data_source/firestore_remote_data_source.dart
@@ -1,11 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 abstract class FireStoreRemoteDataSource {
-  Future<void> UploadDataToFireStore();
+  Future<void> uploadDataToFireStore(
+      {required String collection,
+      required String doc,
+      required Map<String, dynamic> data});
 }
 
 class FireStoreRemoteDataSourceImpl implements FireStoreRemoteDataSource {
+  FirebaseFirestore _fireStore = FirebaseFirestore.instance;
+
   @override
-  Future<void> UploadDataToFireStore() {
-    // TODO: implement UploadDataToFireStore
-    throw UnimplementedError();
+  Future<void> uploadDataToFireStore(
+      {required String collection,
+      required String doc,
+      required Map<String, dynamic> data}) async {
+    await _fireStore.collection(collection).doc(doc).set(data);
   }
 }
