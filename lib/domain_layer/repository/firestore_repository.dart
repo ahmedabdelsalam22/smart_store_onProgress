044dda58e80106727ee87ec398410b31d3d2b99c
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-03-02 17:27:03 +0200

    apply uploadUserDataToFireStore

--- lib/domain_layer/repository/firestore_repository.dart
+++ lib/domain_layer/repository/firestore_repository.dart
@@ -1,3 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../data_layer/data_source/firestore_remote_data_source.dart';
+
 abstract class FireStoreRepository {
   Future<void> uploadUserDataToFireStore(
       {required String collection,
@@ -6,10 +10,21 @@ abstract class FireStoreRepository {
 }
 
 class FireStoreRepositoryImpl implements FireStoreRepository {
+  final FireStoreRemoteDataSource _remoteDataSource;
+  FireStoreRepositoryImpl(this._remoteDataSource);
+
+  final _currentUser = FirebaseAuth.instance.currentUser;
+
   @override
   Future<void> uploadUserDataToFireStore({
     required String collection,
     required String doc,
     required Map<String, dynamic> data,
-  }) async {}
+  }) async {
+    await _remoteDataSource.uploadDataToFireStore(
+      collection: 'users',
+      doc: _currentUser!.uid,
+      data: data,
+    );
+  }
 }
