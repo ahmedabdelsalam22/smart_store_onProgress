50bcf6ce1606bb2d33a6cbade26124d2b8b43920
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-03-02 10:52:31 +0200

    refactor remote_data_source.dart and authRepository

--- lib/data_layer/data_source/remote_data_source.dart
+++ lib/data_layer/data_source/remote_data_source.dart
@@ -1,54 +1,40 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class RemoteDataSource {
-  Future<void> createUserWithEmailAndPassword(
+  Future<User?> createUserWithEmailAndPassword(
       {required String emailAddress, required String password});
 
-  Future<void> signInWithEmailAndPassword(
+  Future<User?> signInWithEmailAndPassword(
       {required String emailAddress, required String password});
 
   Future<void> signOut();
 }
 
 class RemoteDataSourceImpl implements RemoteDataSource {
+  final _firebaseAuth = FirebaseAuth.instance;
+
   @override
-  Future<void> createUserWithEmailAndPassword(
+  Future<User?> createUserWithEmailAndPassword(
       {required String emailAddress, required String password}) async {
-    try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: emailAddress,
-        password: password,
-      );
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
-      } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
-      }
-    } catch (e) {
-      print(e);
-    }
+    var userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+      email: emailAddress,
+      password: password,
+    );
+    return userCredential.user;
   }
 
   @override
-  Future<void> signInWithEmailAndPassword(
+  Future<User?> signInWithEmailAndPassword(
       {required String emailAddress, required String password}) async {
-    try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: emailAddress,
-        password: password,
-      );
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        print('No user found for that email.');
-      } else if (e.code == 'wrong-password') {
-        print('Wrong password provided for that user.');
-      }
-    }
+    var userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+      email: emailAddress,
+      password: password,
+    );
+    return userCredential.user;
   }
 
   @override
   Future<void> signOut() async {
-    await FirebaseAuth.instance.signOut();
+    await _firebaseAuth.signOut();
   }
 }
